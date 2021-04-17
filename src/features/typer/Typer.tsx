93c87d06d94e3acd6039000fe6080595e17819c1
dawid <dtomaszewski88@gmail.com>
2021-04-17 11:45:11 +0200

    add prettier

--- src/features/typer/Typer.tsx
+++ src/features/typer/Typer.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { InputGroup, FormControl, Container } from 'react-bootstrap'
+import { InputGroup, FormControl, Container, Button } from 'react-bootstrap'
 import { getLocalText } from './typerSlice';
 
 declare type FormControlElement = HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement;
@@ -15,9 +15,18 @@ const Typer = ({ localText, remoteText, currentWord, updateLocalText }: Props) =
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = e.currentTarget.value;
     updateLocalText(newValue)
-}
-
+  }
+  const handleConnect = () => {
+    console.log('handleConnect')
+  }
+  const handleAddToQueue = () => {
+    console.log('handleAddToQueue')
+  }
+  const handleReady = () => {
+    console.log('handleStart')
+  }
   return (
+    <>
     <Container>
       <h1>Current Word: {currentWord}</h1>
       <h1>Local Text: {localText}</h1>
@@ -34,7 +43,13 @@ const Typer = ({ localText, remoteText, currentWord, updateLocalText }: Props) =
         aria-describedby="basic-addon1"
       />
       </InputGroup>
-    </Container>
+      </Container>
+      <Container>
+        <Button variant="primary" onClick={handleConnect}>Connect</Button>{' '}
+        <Button variant="secondary" onClick={handleAddToQueue}>Add To queue</Button>{' '}
+        <Button variant="success" onClick={handleReady}>Ready</Button>{' '}
+      </Container>
+      </>
   )
 }
 export default Typer
